04be49de2747ecd2831f2d80d86c44a6dc051ac6
Rahul <rahulscodingworld@gmail.com>
2022-09-04 21:49:29 +0530

    rendering profiles

--- api.js
+++ api.js
@@ -1,5 +1,11 @@
 import { createClient } from "urql"
 
+const APIURL = "https://api.lens.dev"
+
+export const client = createClient({
+  url: APIURL,
+})
+
 export const recommendedProfiles = `
 query RecommendedProfiles {
     recommendedProfiles {
