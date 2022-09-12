a6b1665c2c1fe57b2df6ae251e9b00dd244fc597
Rahul <rahulscodingworld@gmail.com>
2022-09-12 12:53:29 +0530

    finally got messaging set up

--- components/Messages.js
+++ components/Messages.js
@@ -8,30 +8,45 @@ export default function Message() {
 
     const {isConnected} = useAccount()
     const { data:signer } = useSigner()
+    const [message, setMessage] = useState('')
+    const [xmtpClient, setXmtpClient] = useState(null)
   
     
     const createDms = async () => {
       
       if (signer) {
-      const xmtp = await Client.create(signer)
-      
-      const conversation = await xmtp.conversations.newConversation(
-        '0x05F0D0CcC2b00f55ea61684Ae2b9369e5e499F91'
-        )
-        
-        const messages = await conversation.messages()
-        console.log(messages)
-
-        await conversation.send("Idk")
-
-        console.log(messages)
-        for await (const message of await conversation.streamMessages()) {
+        const xmtp = await Client.create(signer)
+        setXmtpClient(xmtp)
+        const conversation = await xmtp.conversations.newConversation(
+          '0x05F0D0CcC2b00f55ea61684Ae2b9369e5e499F91'
+          )
+        for await (const message of await conversation.streamMessages()) {  
           console.log(`[${message.senderAddress}]: ${message.content}`)
         }
       }
+    }
+    
+    const sendMessage = async () => {
+      if (signer) {
+        const conversation = await xmtpClient.conversations.newConversation(
+          '0x05F0D0CcC2b00f55ea61684Ae2b9369e5e499F91'
+          )
+          await conversation.send(message)
+          setMessage('')
+        }
+      }
+
+      const onChange =(e) => {
+        e.preventDefault()
+        setMessage(e.target.value) 
+      }
+
+
+      const onSubmit = (e) => {
+        e.preventDefault()
+        sendMessage()
 
       }
-      
       
       useEffect(() => {
         if (isConnected) {
@@ -40,5 +55,17 @@ export default function Message() {
       }, [isConnected, signer])
 
 
+
+
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+              <input onChange={onChange} type="text" placeholder="Message" />
+              <button type="submit">Send</button>
+            </form>
+        </div>
+    )
 }
+
+
 
